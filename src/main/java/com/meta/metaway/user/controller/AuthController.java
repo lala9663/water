faf016438b4dae7f2d72326f36ed1f38504536eb
lala9663 <lala9663@naver.com>
2024-01-07 15:52:33 +0900

    컨트롤러 부분 restcontroller -> controller로 이동중,,

--- src/main/java/com/meta/metaway/user/controller/AuthController.java
+++ src/main/java/com/meta/metaway/user/controller/AuthController.java
@@ -1,32 +1,81 @@
 package com.meta.metaway.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.meta.metaway.jwt.JWTUtil;
 import com.meta.metaway.user.dto.JoinDTO;
+import com.meta.metaway.user.model.User;
 import com.meta.metaway.user.service.IUserService;
 
-@RestController
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
 public class AuthController {
 
 	@Autowired
-	IUserService userService;
+    private IUserService userService;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 
+    @GetMapping("/")
+    public String index() {
+    
+    	return "index"; 
+    }
+
+    @GetMapping("/join")
+    public String getJoinPage(Model model) {
+        model.addAttribute("joinDTO", new JoinDTO());
+
+        return "join"; 
+    }
 
     @PostMapping("/join")
-    public ResponseEntity<String> joinUser(@RequestBody JoinDTO joinDTO) {
-        boolean isExist = userService.checkIfUserExists(joinDTO.getAccount());
-
-        if (isExist) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재합니다");
-        } else {
-            userService.joinProcess(joinDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입되었습니다");
-        }
+    public String joinUser(JoinDTO joinDTO) {
+        userService.joinProcess(joinDTO);
+        
+        return "redirect:/login"; 
     }
+    
+	@GetMapping("/login")
+	public String login() {
+		  
+	 return "/member/login";
+	}
+	
+	@GetMapping("/user/profile")
+	public String goProfilePage() {
+	   
+	 return "member/profile";
+	}
+
+	@PostMapping("/user/profile")
+	public String getProfilePage(HttpServletRequest request, Model model) {
+	    String tokenHeader = request.getHeader("Authorization");
+	    String token = null;
+
+	    if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+	        token = tokenHeader.substring(7); 
+	    }
+	    System.out.println(token);
+	    if (token != null) {
+	        String username = jwtUtil.getUsername(token);
+	        User user = userService.getUserByUsername(username);
+
+	        if (user != null) {
+	            model.addAttribute("userProfile", user);
+	            return "member/profile";
+	        }
+	    }
+
+	    // 사용자 정보가 없는 경우 로그인 페이지로 리다이렉트 또는 에러 페이지 등을 처리할 수 있습니다.
+	    return "redirect:/login";
+	}
 
 }
