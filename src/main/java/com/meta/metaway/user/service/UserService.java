445290649650ad630095478fb776ebf3631f2d38
jgong4694 <https://github.com/MinsunCho-dev/TeamPage>
2024-01-10 15:21:41 +0900

    유저 서비스 장바구니 이미지 스트링 변경기능 추가

--- src/main/java/com/meta/metaway/user/service/UserService.java
+++ src/main/java/com/meta/metaway/user/service/UserService.java
@@ -1,4 +1,7 @@
 package com.meta.metaway.user.service;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -128,6 +131,7 @@ public class UserService implements IUserService {
     	List<Product> productList = basketRepository.getBasketItemsByUserId(userId);
     	for(Product product : productList) {
     		product.setFunctionList(productRepository.getProductKey(product.getProductId()));
+    		product.setImageFile(productImageString(product.getImageFile()));
     	}
         return productList;
     }
@@ -155,4 +159,18 @@ public class UserService implements IUserService {
     	
         userRepository.deleteUserById(id);
     }
+    
+    
+	private String productImageString(String filePath) {
+		try {
+			InputStream input = new FileInputStream(filePath);
+		
+		byte[] byteFile = input.readAllBytes();
+		String encodedByte = Base64.getEncoder().encodeToString(byteFile);
+		return encodedByte;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
 }
