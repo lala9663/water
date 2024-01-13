6c19fc6844ff50ce671770600479d02ad1756c3b
lala9663 <lala9663@naver.com>
2024-01-13 17:38:32 +0900

    Feat: 빠진 DTO 부분 추가

---
+++ src/main/java/com/meta/metaway/admin/dto/UserCountDTO.java
@@ -0,0 +1,11 @@
+package com.meta.metaway.admin.dto;
+
+import lombok.Data;
+
+@Data
+public class UserCountDTO {
+	private int totalAllCount;
+	private int totalUserCount;
+	private int totalCodiCount;
+	private int totalDriverCount;
+}
