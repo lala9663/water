e269296b57eba737aa5a38591e080b01d9117926
lala9663 <lala9663@naver.com>
2024-01-14 02:55:19 +0900

    feat: 12시 전에 스케줄러를 통한 자동 디비 저장

--- src/main/java/com/meta/metaway/admin/scheduler/VisitorScheduler.java
+++ src/main/java/com/meta/metaway/admin/scheduler/VisitorScheduler.java
@@ -6,18 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.meta.metaway.admin.dao.IAdminRepository;
+import com.meta.metaway.admin.model.Visitor;
 import com.meta.metaway.admin.service.AdminService;
+import com.meta.metaway.admin.service.IAdminService;
 
 @Component
 public class VisitorScheduler {
 
     @Autowired
-    private AdminService adminService;
+    private IAdminService adminService;
+    @Autowired
+    private IAdminRepository adminRepository;
 
     // 매일 자정에 실행
     @Scheduled(cron = "0 0 0 * * *")
     public void resetDailyVisitorCount() {
         // 방문자 수 초기화
         String key = AdminService.DAILY_VISITOR_COUNT_KEY + LocalDate.now();
-        adminService.resetDailyVisitorCount(key);    }
+        adminService.resetDailyVisitorCount(key);    
+    }
+    
+    
+    // 매일 11시 59분에 실행
+    @Scheduled(cron = "0 59 11 * * *")
+    public void saveDailyVisitorCountToDatabase() {
+
+    	long dailyVisitorCount = adminService.getDailyVisitorCount();
+        long visitorId = adminRepository.selectNextVisitorId();
+
+        Visitor visitor = new Visitor();
+        visitor.setVisitorId(visitorId);
+        visitor.setVisitDate(LocalDate.now());
+        visitor.setVisitorCount(dailyVisitorCount);
+
+        adminService.insertViewCount(visitor);
+    }
+
+    
 }
